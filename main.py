e9969a70c88c3929bfd09caf8387d432cd75b051
Arian <aryan23@aut.ac.ir>
2024-04-25 20:16:29 -0400

    fix: full pipeline

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 from utils import (
     get_image_as_array,
     get_hilbert_curve_points,
-    huffman_decoding,
-    huffman_encoding,
     move_to_front_decoding,
     move_to_front_encoding,
+    huffman_encoding,
+    huffman_decoding,
     run_length_encoding,
     run_length_decoding,
     save_array_as_image,
@@ -38,19 +38,26 @@ def compress_image(image):
     encoded_image = np.zeros((linearized_image_length, image_c), dtype=np.uint8)
     for i in range(image_c):
         encoded_image[:, i] = move_to_front_encoding(linearized_image[:, i])
-    
+
     # Huffman encoding for each channel
     huffman_encoded_image = [[] for _ in range(image_c)]
     huffman_codes = [[] for _ in range(image_c)]
     for i in range(image_c):
         huffman_encoded_image[i], huffman_codes[i] = huffman_encoding(encoded_image[:, i])
 
-    with open("compressed_image", "wb") as f:
-        pickle.dump({
-            "huffman_encoded_image": huffman_encoded_image,
-            "huffman_codes": huffman_codes,
-            "order": order
-        }, f)
+    initial_img_size = image_w * image_h * image_c * 8
+    compressed_img_size = sum([len(huffman_encoded_image[i]) for i in range(image_c)]) + sum([len(huffman_codes[i]) for i in range(image_c)]) * 2 * 8
+
+    print(f"Initial file size: {initial_img_size} bits")
+    print(f"Compressed file size: {compressed_img_size} bits")
+    print(f"Compression ratio: {initial_img_size / compressed_img_size:.2f}")
+
+    # with open("compressed_image", "wb") as f:
+    #     pickle.dump({
+    #         "huffman_encoded_image": huffman_encoded_image,
+    #         "huffman_codes": huffman_codes,
+    #         "order": order
+    #     }, f)
 
 
 def decompress_image(image):
@@ -64,6 +71,23 @@ def decompress_image(image):
     for i in range(image_c):
         decoded_image[i] = huffman_decoding(huffman_encoded_image[i], huffman_codes[i])
         decoded_image[i] = move_to_front_decoding(decoded_image[i])
+
+    decoded_image = np.array(decoded_image)
+
+    N = 2**order
+
+    reconstructed_image = np.zeros((N, N, image_c), dtype=np.uint8)
+
+    h_x, h_y = get_hilbert_curve_points(order=order)
+    h_x = h_x.astype(int)
+    h_y = h_y.astype(int)
+
+    for i in range(N * N):
+        reconstructed_image[h_y[i], h_x[i], :] = decoded_image[:, i]
+
+    reconstructed_image = reconstructed_image[::-1, :, :]
+
+    save_array_as_image(reconstructed_image, "decompressed_image.png")
     
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from PIL import Image
+from collections import Counter
+from heapq import heapify, heappop, heappush
 import numpy as np
-import pickle
 
 
 def get_image_as_array(image_path):
@@ -73,70 +74,81 @@ def run_length_decoding(encoded_text):
     return "".join(result)
 
 
-def huffman_encoding(text):
-    freq = {}
-    for char in text:
-        if char in freq:
-            freq[char] += 1
-        else:
-            freq[char] = 1
-
-    freq = {k: v for k, v in sorted(freq.items(), key=lambda item: item[1])}
-
-    nodes = list(freq.keys())
-    while len(nodes) > 1:
-        left = nodes.pop(0)
-        right = nodes.pop(0)
-        nodes.append((left, right))
-
-    huffman_codes = {}
-    def get_codes(node, code=""):
-        if isinstance(node, np.uint8):
-            huffman_codes[node] = code
-        else:
-            get_codes(node[0], code + "0")
-            get_codes(node[1], code + "1")
-
-    get_codes(nodes[0])
-
-    encoded_text = ''.join([huffman_codes[char] for char in text])
-
-    return encoded_text, huffman_codes
-
-
-def huffman_decoding(encoded_text, huffman_codes):
-    huffman_codes = {v: k for k, v in huffman_codes.items()}
-    decoded_text = []
-    code = ""
-    for char in encoded_text:
-        code += char
-        if code in huffman_codes:
-            decoded_text.append(huffman_codes[code])
-            code = ""
-    return decoded_text
-
-
 def get_hilbert_curve_points(order, norm=False, offset=0.0):
-    try:
-        with open(f"hilbert_points", "rb") as f:
-            points = pickle.load(f)
-            points = points[order]
-        return points
-    except FileNotFoundError:
-        N = 2**order
-        points = np.zeros((2, N * N))
-        for i in range(N * N):
-            U = np.array([[0, 0], [0, 1], [1, 1], [1, 0]])   # four points under ORDER=1
-            V = np.array([offset, offset])                   # offset: starting point
-            for j in reversed(range(order)):
-                index = i // 4**j % 4
-                length = 2**j
-                if index == 0:
-                    U[1], U[3] = U[3].copy(), U[1].copy() 
-                elif index == 3:
-                    U[0], U[2] = U[2].copy(), U[0].copy()
-                V += U[index] * length
-            points[:, i] = V
-        if norm:
-            points /= N
-        return points
+    N = 2**order
+    points = np.zeros((2, N * N))
+
+    for i in range(N * N):
+        U = np.array([[0, 0], [0, 1], [1, 1], [1, 0]])
+        V = np.array([offset, offset])
+        for j in reversed(range(order)):
+            index = i // 4**j % 4
+            length = 2**j
+            if index == 0:
+                U[1], U[3] = U[3].copy(), U[1].copy() 
+            elif index == 3:
+                U[0], U[2] = U[2].copy(), U[0].copy()
+            V += U[index] * length
+        points[:, i] = V
+
+    if norm:
+        points /= N
+
+    return points
+
+
+class Node:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+
+def huffman_encoding(data):
+  char_freq = Counter(data)
+
+  nodes = [Node(char, freq) for char, freq in char_freq.items()]
+  
+  heapify(nodes)
+
+  while len(nodes) > 1:
+    left, right = heappop(nodes), heappop(nodes)
+
+    merged = Node(None, left.freq + right.freq)
+    merged.left = left
+    merged.right = right
+    heappush(nodes, merged)
+
+  codes = {}
+
+  def traverse(node, prefix="", code={}):
+    if node is not None:
+        if node.char is not None:
+            code[node.char] = prefix
+        traverse(node.left, prefix + '0', code)
+        traverse(node.right, prefix + '1', code)
+    return code
+
+  codes = traverse(nodes[0], "")
+
+  encoded_data = "".join(codes[char] for char in data)
+
+  return encoded_data, codes
+
+
+def huffman_decoding(data, codes):
+    reverse_codes = {v: k for k, v in codes.items()}
+    current_code = ""
+    decoded_text = ""
+
+    for bit in data:
+        current_code += bit
+        if current_code in reverse_codes:
+            decoded_text += reverse_codes[current_code]
+            current_code = ""
+    
+    return decoded_text
